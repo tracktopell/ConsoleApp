1538294365c46f0b2cea8d97d3bf5e86e6177bb8
U-SFP\upcp-soporte18 <upcp-soporte18@X1-16-SOPORTE18.sfp.gob.mx>
2019-04-26 13:55:20 -0500

    now real run from command line and dist is ready for distribution

--- src/main/java/com/tracktopell/app/consoleapp/Main.java
+++ src/main/java/com/tracktopell/app/consoleapp/Main.java
@@ -2,6 +2,10 @@ package com.tracktopell.app.consoleapp;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Map;
+import java.io.IOException;
+
+import org.apache.commons.exec.environment.EnvironmentUtils;
 
 /**
  *
@@ -11,7 +15,14 @@ public class Main {
     public static void main(String[] args) {
         SimpleDateFormat sdf = new SimpleDateFormat("YYYY-MM-dd HH:mm:ss.SSS");
         Date hoy = new Date();
+		Map<String,String> propEnvMap = null;
+		try{
+			propEnvMap = EnvironmentUtils.getProcEnvironment();
+		} catch(IOException ioe){
+			System.err.println("-->> error getting Environment vars :( ");
+		}
         System.out.println("["+sdf.format(hoy)+"]-->>"+Main.class.getName());
+        System.out.println("["+sdf.format(hoy)+"]-->>"+propEnvMap);
     }
     
 }
